a5e971818583ad5faacc28ede13092b509d28aca
Abhinav Verma <abhinav_win12@yahoo.co.in>
2021-10-06 10:39:53 +0530

    add upload artifact

---
+++ upload_artifact.py
@@ -0,0 +1,51 @@
+import wandb
+import pathlib
+import argparse
+from logger import setup_logger
+log = setup_logger(__name__,"upload_artifact.log")
+
+
+def go(args):
+
+    log.info("Creating run exercise_1")
+    run = wandb.init(project="exercise_1", job_type="upload_file")
+
+    log.info("Creating artifact")
+    artifact = wandb.Artifact(
+        name=args.artifact_name,
+        type=args.artifact_type,
+        description=args.artifact_description,
+    )
+    artifact.add_file(args.input_file)
+
+    log.info("Logging artifact")
+    run.log_artifact(artifact)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Upload an artifact to W&B", fromfile_prefix_chars="@"
+    )
+
+    parser.add_argument(
+        "--input_file", type=pathlib.Path, help="Path to the input file", required=True
+    )
+
+    parser.add_argument(
+        "--artifact_name", type=str, help="Name for the artifact", required=True
+    )
+
+    parser.add_argument(
+        "--artifact_type", type=str, help="Type for the artifact", required=True
+    )
+
+    parser.add_argument(
+        "--artifact_description",
+        type=str,
+        help="Description for the artifact",
+        required=True,
+    )
+
+    args = parser.parse_args()
+
+    go(args)
